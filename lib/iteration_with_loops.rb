8d7b77303fd6f888f314cf4d62598ef04fae9733
Anna Nicole Reed <reedanna@mail.com>
2020-06-22 20:15:10 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,9 +6,12 @@ def join_nested_strings(src)
   while outer_counter < src.length do
     inner_counter = 0
     while inner_counter < src[outer_counter].length do
-      if src[outer_counter][inner_counter].class == 
+      if src[outer_counter][inner_counter].class == String
+        strings = strings + src[outer_counter][inner_counter]
+      end
       inner_counter += 1
     end
     outer_counter += 1
   end
+  strings
 end
